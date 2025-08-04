739196dcce76d0535b04c4e4e9f22c9a0d58a29c
preetbhingradiya <preetbhingradiya6@example.com>
2025-08-04 23:13:41 +0530

    changes

--- frontend/src/components/MainMap.tsx
+++ frontend/src/components/MainMap.tsx
@@ -189,7 +189,7 @@
 
 import { useEffect, useRef } from "react";
 import L from "leaflet";
-import "leaflet.heat";
+import "leaflet.heat"; 
 import "leaflet/dist/leaflet.css";
 import { v4 as uuidv4 } from 'uuid';
 
